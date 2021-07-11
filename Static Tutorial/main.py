90033916cba95470530cf157f47e5e793aa4833f
charss <charleskylereyes16@gmail.com>
2021-07-11 09:54:36 +0800

    Added blueprints

--- Static Tutorial/main.py
+++ Static Tutorial/main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+from admin.second import second
 
 app = Flask(__name__)
+app.register_blueprint(second, url_prefix='/admin')
 
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return "<h1>HOME</h1>"
 
 if __name__ == '__main__':
     app.run(debug=True)
